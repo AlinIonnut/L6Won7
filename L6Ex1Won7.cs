a9ebb0aeb1ac2a0bad10a6787f18fb948fc208ca
Scarlat Alin <91609566+AlinIonnut@users.noreply.github.com>
2024-04-02 09:43:52 +0300

    Add files via upload

---
+++ L6Ex1Won7.cs
@@ -0,0 +1,24 @@
+﻿
+Closet Bucatarie = new Closet(10, 20, 30);
+
+Console.WriteLine(Bucatarie.VolumOfCloset());
+
+class Closet
+{
+    readonly int lenght;
+    readonly int width;
+    readonly int height;
+
+    public Closet(int lenght, int width, int height)
+    {
+        this.lenght = lenght;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int VolumOfCloset()
+    {
+        int solution = lenght * width * height;
+        return solution;
+    }
+}
---
+++ L6Ex2Won7.cs
@@ -0,0 +1,25 @@
+﻿
+Car FirstCar = new Car("Honda", "BV 43 ERG", 1998) ;
+Car SecondCar = new Car("Ford", "CT 341 DSA", 1598);
+
+Console.WriteLine(FirstCar.Display());
+Console.WriteLine(SecondCar.Display());
+
+class Car
+{
+    string model;
+    string registrationNumber;
+    int engine;
+
+    public Car(string model, string registrationNumber, int engine)
+    {
+        this.model = model;
+        this.registrationNumber = registrationNumber;
+        this.engine = engine;
+    }
+
+    public string Display()
+    {
+        return ($"John just got a new car. The car is a {model} and has the cilindrical capacity of {engine}. He also got a new licence plate, called {registrationNumber}. ");
+    }
+}
